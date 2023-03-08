71c0a29771f4c654f0a48f873186e5619926b515
chettrich15 <103069852+chettrich15@users.noreply.github.com>
2023-03-08 14:46:34 -0500

    Update ISYE-3133-PA02.py

--- ISYE-3133-PA02.py
+++ ISYE-3133-PA02.py
@@ -1,3 +1,9 @@
+# Emily McNichols and Charlotte Hettrich
+# ISYE 3133
+# Dr. Strickland
+# PA02
+# Due: March 12, 2023
+
 # Import statements
 import pandas as pd
 from gurobipy import *
@@ -7,7 +13,6 @@ demand_data_df = pd.read_csv('demand_data.csv', index_col = 'Month')
 cost_data_df = pd.read_csv('cost_data.csv', index_col = 'Month')
 product_data_df = pd.read_csv('product_data.csv')
 volume_data_df = pd.read_csv('volume_data.csv')
-
 
 # Retrieve name of the list
 months = cost_data_df.index.tolist()
@@ -20,7 +25,6 @@ total_volume = volume_data_df.at[0,'Total volume']
 
 # Build Model
 m = Model()
-m.update()
 
 # Number of months and products
 num_months = len(months)
@@ -30,12 +34,13 @@ num_products = len(products)
 x = m.addVars(num_products, num_months, name = 'x')
 inv = m.addVars(num_products, num_months, name = 'I')
 
-# Set objective
+# Set objective function
 m.setObjective(quicksum(cost_data_df.iloc[t][i] * x[i, t] for i in range(num_products) for t in range(num_months))
 	+ quicksum(storage_cost_per_product[i] * inv[i, t] for i in range(num_products) for t in range(num_months - 1)), GRB.MINIMIZE)
 
 # Inventory constraint for t = 0
 m.addConstrs(inv[i, 0] - x[i, 0] + demand_data_df.iloc[0][i] == 0 for i in range(num_products))
+m.update()
 
 # Inventory constraints for t > 0
 for t in range(1, num_months):
@@ -46,16 +51,23 @@ for t in range(1, num_months):
 for t in range(num_months):
 	m.addConstr(quicksum(volumes_per_product[i] * inv[i, t] for i in range(num_products)) <= total_volume)
 	m.update()
-    
 
+# Optimize the model
 m.optimize()
 
-# View optimal solution
-print('Obj: %g' % m.objVal)
-# for v in m.getVars():
-#     print('%s %g' % (v.varName, v.x))
-    
+# Print the result
+status_code = {1: 'LOADED', 2: 'OPTIMAL', 3: 'INFEASIBLE', 4: 'INF_OR_UNBD', 5: 'UNBOUNDED'}
+status = m.status
 
+print("The optimization status is {}".format(status_code[status]))
+if status == 2:
+# Retrieve variables value
+    print("Optimal solution:")
+    for v in m.getVars():
+        print("%s = %g" % (v.varName, v.x))
+    print("Optimal objective value:\n{}".format(m.objVal))
+    
+# Putting the optimal solution into dataframes for readability
 vars_list = m.getVars();
 x_df = pd.DataFrame(index = range(num_months))
 x_df.index.name = 'Month'
@@ -70,7 +82,5 @@ inv_index = num_products*num_months
 for i in range(num_products):
 	inv_df['Product %d' % i] = [v.x for v in vars_list[i * num_months + inv_index: i * num_months + num_months + inv_index]]
 
-
-
-
-
+print(x_df)
+print(inv_df)
